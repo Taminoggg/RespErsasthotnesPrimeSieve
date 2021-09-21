bebcff949a3edcd058d938b021d9bfda5dbd9397
Tamino <gutjahr.taminogmx.at>
2021-09-21 11:12:11 +0200

    print all Primes done

---
+++ ErastosthenesPrimeSieve/src/erastosthenesprimesieve/ErastosthenesPrimeSieve.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package erastosthenesprimesieve;
+
+/**
+ *
+ * @author Silke
+ */
+public class ErastosthenesPrimeSieve {
+    
+    double MAX;
+
+    public ErastosthenesPrimeSieve(int max){
+        this.MAX = max;
+    }
+    
+    public boolean isPrime(int i){
+        boolean isPrime = false;
+        return isPrime;
+    }
+    
+    public void printPrimes(){
+        
+        boolean[] notPrime = new boolean[Integer.parseInt(MAX-2.0)];
+        
+        for(int i=2;i<MAX;i++){
+           notPrime[i] = false;
+        }
+        
+        for(int i=2;i<Math.sqrt(MAX);i++){
+            if(!notPrime[i]){
+                System.out.println(i);
+            }
+            for(int j=i*i;j<MAX;i++){
+                notPrime[j] = true;
+            }
+        }
+        
+        for(int i= Integer.parseInt(Math.sqrt(MAX));i<MAX;i++){
+            if(!notPrime[i]){
+                System.out.println(i);
+            }
+        }
+    }
+}
